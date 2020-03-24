d6dbd105a016eeacb6dcca9d5933c53a7c6093ff
lemon-clown <me.clown.lemon@gmail.com>
2020-03-24 19:58:26 +0800

    :construction:  [laputa] feat: add route for FlexBasisTest

--- stage/laputa/src/routes/index.tsx
+++ stage/laputa/src/routes/index.tsx
@@ -2,10 +2,12 @@ import { RouteItemData } from '../app'
 import HelloWorldRoute from './hello-world'
 import RouteTreeRoute from './route-tree'
 import BoxPercentageTest from './test/percentage'
+import FlexBasisTest from './test/flexbox/flex-basis'
 
 
 export default [
   HelloWorldRoute,
   RouteTreeRoute,
   BoxPercentageTest,
+  FlexBasisTest,
 ] as RouteItemData[]
