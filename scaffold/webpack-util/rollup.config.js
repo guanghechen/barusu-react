93f3f8333d7e7674302ee51781c927fc3d9a8ef8
lemon-clown <me.clown.lemon@gmail.com>
2020-03-29 18:55:16 +0800

    :recycle:  [webpack-util] refactor: use rollup to bundle cjs

---
+++ scaffold/webpack-util/rollup.config.js
@@ -0,0 +1,38 @@
+import path from 'path'
+import { createRollupConfig } from '@barusu/rollup-config'
+import manifest from './package.json'
+
+
+const resolvePath = p => path.resolve(__dirname, p)
+const paths = {
+  eslintrc: resolvePath('.eslintrc.js'),
+  tsconfig: resolvePath('tsconfig.json'),
+}
+
+
+const config = createRollupConfig({
+  manifest,
+  external: [
+    'fs',
+    'path',
+    'os',
+    ...Object.keys(manifest.dependencies),
+  ],
+  pluginOptions: {
+    eslintOptions: {
+      configFile: paths.eslintrc,
+    },
+    typescriptOptions: {
+      tsconfig: paths.tsconfig,
+      useTsconfigDeclarationDir: true,
+    },
+    commonjsOptions: {
+      include: ['../../node_modules/**'],
+      namedExports: {
+      },
+    },
+  }
+})
+
+
+export default config
