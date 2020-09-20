ec2165c45ae50856567461311774ef28b4b77570
lemon-clown <me.clown.lemon@gmail.com>
2020-09-20 23:16:41 +0800

    :construction:  [eslint-config] feat: upgrade dependencies & update eslint configs

--- scaffold/eslint-config/index.js
+++ scaffold/eslint-config/index.js
@@ -1,63 +1,131 @@
 module.exports = {
-  'extends': [
-    'react-app',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint'
-  ],
-  'parser': '@typescript-eslint/parser',
-  'plugins': [
+  root: true,
+  extends: ['react-app'],
+  plugins: [
     '@typescript-eslint',
+    'import',
+    'flowtype',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
     'prettier'
   ],
-  'settings': {
-    'import/resolver': {
-      'node': {
-        'extensions': ['.js', '.ts', '.jsx', '.tsx']
+  overrides: [
+    {
+      files: ['**/*{.ts,tsx}'],
+      extends: [
+        'react-app',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint'
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/interface-name-prefix': 0,
+        '@typescript-eslint/no-empty-function': 0,
+        '@typescript-eslint/no-empty-interface': 0,
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-non-null-assertion': 0,
+        '@typescript-eslint/no-this-alias': [
+          'error',
+          {
+            'allowDestructuring': true, // Allow `const { props, state } = this`; false by default
+            'allowedNames': ['self']    // Allow `const self = this`; `[]` by default
+          }
+        ],
+        '@typescript-eslint/space-before-function-paren': [
+          'error',
+          {
+            'named': 'never',
+            'anonymous': 'always',
+            'asyncArrow': 'always',
+          }
+        ],
       }
-    }
-  },
-  'ignorePatterns': [
-    'lib/',
-    'dist/',
-    'build/',
-    'bundle/',
-    'target/',
-    'release/',
-    'node_modules/',
-    '*.tsbuildinfo',
-    '**/*.styl.d.ts',
-    "rollup.config.js"
+    },
   ],
-  'rules': {
+  ignorePatterns: [
+    '**/test/cases/**',
+    '**/__test__/cases/**',
+    '**/node_modules/**',
+    '**/lib/**',
+    '**/dist/**',
+    '**/build/**',
+    '**/target/**',
+    '**/vendor/**',
+    '**/release/**',
+    '**/example/**',
+    '**/demo/**',
+    '**/doc/**',
+    '**/tmp/**',
+    '**/__tmp__/**',
+    '**/coverage/**',
+    '**/*.styl.d.ts',
+    '*.tsbuildinfo',
+  ],
+  rules: {
     'class-methods-use-this': 0,
+    'func-call-spacing': ['error', 'never'],
     'func-names': 0,
-    'import/prefer-default-export': 0,
-    'import/no-unresolved': 0,
+    'key-spacing': ['error'],
     'lines-between-class-members': 0,
+    'max-len': [
+      'error',
+      {
+        'code': 100,
+        'comments': 100,
+        'tabWidth': 2,
+        'ignoreTrailingComments': true,
+        'ignoreUrls': true,
+        'ignoreStrings': true,
+        'ignoreTemplateLiterals': true,
+        'ignoreRegExpLiterals': true,
+      }
+    ],
+    'new-cap': ['error', { 'newIsCap': true, 'capIsNew': true }],
     'no-await-in-loop': 0,
     'no-bitwise': 0,
     'no-console': 0,
     'no-continue': 0,
-    'no-cond-assign': 0,
-    'no-inner-declarations': 0,
+    'no-cond-assign': ['error', 'always'],
+    'no-inner-declarations': 'error',
+    'no-mixed-operators': 'error',
+    'no-mixed-spaces-and-tabs': 'error',
+    'no-multi-spaces': ['error', { 'ignoreEOLComments': true }],
     'no-param-reassign': ['error', { 'props': true }],
-    'no-plusplus': 0,
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
     'no-restricted-syntax': 0,
-    'no-return-assign': 0,
+    'no-return-assign': ['error', 'always'],
     'no-throw-literal': 0,
     'no-underscore-dangle': 0,
     'prefer-destructuring': 0,
     'quotes': ['error', 'single'],
-    'react/jsx-filename-extension': [2, { extensions: ['.tsx'] }],
+    'react/jsx-filename-extension': [2, { extensions: ['.jsx', '.tsx'] }],
     'react/jsx-indent-props': [2, 2],
     'react/jsx-indent': [2, 2],
     'semi': ['error', 'never'],
-    "@typescript-eslint/interface-name-prefix": 0,
-    '@typescript-eslint/no-empty-function': 0,
-    '@typescript-eslint/no-empty-interface': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/no-this-alias': 0,
-    '@typescript-eslint/no-use-before-define': 0,
+    'space-before-blocks': [
+      'error',
+      {
+        'functions': 'always',
+        'keywords': 'always',
+        'classes': 'always',
+      }
+    ],
+    'space-before-function-paren': 0,
+    'spaced-comment': ['error', 'always'],
+    'space-in-parens': ['error', 'never'],
+    'space-infix-ops': ['error', { 'int32Hint': false }],
+    'space-unary-ops': ['error', { 'words': true, 'nonwords': false }],
   }
 }
